2a2b960a06cc3033108816cb0960f697dbef5e6e
guruece24 <guruece24@gmail.com>
2022-07-14 10:36:16 +0530

    typedJsaddition

--- js/app.js
+++ js/app.js
@@ -13,4 +13,14 @@ $(document).ready(function(){
     }
   });
 
+    var typed = new Typed(".telement", {
+    strings: ['Peter', 'SEO Expert', 'a Designer', 'a Manager', 'a Business Specialist'],
+    smartBackspace: true,
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+    loopCount: Infinity,
+    startDelay: 1000
+  });
+
 });
