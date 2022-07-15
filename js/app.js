7803e6ff28f38323cc2e6e524a70f4988ffa5d3a
guruece24 <guruece24@gmail.com>
2022-07-15 10:20:29 +0530

    services

--- js/app.js
+++ js/app.js
@@ -23,4 +23,17 @@ $(document).ready(function(){
     startDelay: 1000
   });
 
+  var waypoint = new Waypoint({
+    element: document.getElementById('exp-id'),
+    handler: function() {
+      var p = document.querySelectorAll('.progress-bar');
+      p[0].setAttribute("style", "width:95%; transition:1s all");
+      p[1].setAttribute("style", "width:90%; transition:1.5s all");
+      p[2].setAttribute("style", "width:98%; transition:2s all");
+      p[3].setAttribute("style", "width:95%; transition:2.3s all");
+      p[4].setAttribute("style", "width:90%; transition:2.5s all");
+    },
+    offset: '90%'
+  });
+
 });
